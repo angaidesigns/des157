b482cc1e84e531906ce87fe01bc0e613a50c62a9
angaidesigns <angaidesigns@gmail.com>
2017-02-07 12:18:16 -0800

    studio3 update

--- studio3/script.js
+++ studio3/script.js
@@ -20,6 +20,13 @@ document.addEventListener("DOMContentLoaded", function() {
         london.style.opacity = 0;
         london.src = 'images/london.png';
     });
+    london.addEventListener('mouseover', function() {
+        londoninfo.style.opacity= 1;
+    });
+    london.addEventListener('mouseout', function() {
+        londoninfo.style.opacity= 0;
+    });
+
 
     italy.addEventListener('mouseover', function() {
         italy.style.opacity = 1;
@@ -29,6 +36,13 @@ document.addEventListener("DOMContentLoaded", function() {
         italy.style.opacity = 0;
         italy.src = 'images/italy.png';
     });
+    italy.addEventListener('mouseover', function() {
+        italyinfo.style.opacity= 1;
+    });
+    italy.addEventListener('mouseout', function() {
+        italyinfo.style.opacity= 0;
+    });
+
 
     quebec.addEventListener('mouseover', function() {
         quebec.style.opacity = 1;
@@ -38,6 +52,13 @@ document.addEventListener("DOMContentLoaded", function() {
         quebec.style.opacity = 0;
         quebec.src = 'images/quebec.png';
     });
+    quebec.addEventListener('mouseover', function() {
+        quebecinfo.style.opacity= 1;
+    });
+    quebec.addEventListener('mouseout', function() {
+        quebecinfo.style.opacity= 0;
+    });
+
 
     hongkong.addEventListener('mouseover', function() {
         hongkong.style.opacity = 1;
@@ -47,6 +68,13 @@ document.addEventListener("DOMContentLoaded", function() {
         hongkong.style.opacity = 0;
         hongkong.src = 'images/hongkong.png';
     });
+    hongkong.addEventListener('mouseover', function() {
+        hongkonginfo.style.opacity= 1;
+    });
+    hongkong.addEventListener('mouseout', function() {
+        hongkonginfo.style.opacity= 0;
+    });
+
 
     seuol.addEventListener('mouseover', function() {
         seuol.style.opacity = 1;
@@ -56,6 +84,13 @@ document.addEventListener("DOMContentLoaded", function() {
         seuol.style.opacity = 0;
         seuol.src = 'images/seuol.png';
     });
+    seuol.addEventListener('mouseover', function() {
+        seuolinfo.style.opacity= 1;
+    });
+    seuol.addEventListener('mouseout', function() {
+        seuolinfo.style.opacity= 0;
+    });
+
 
     paris.addEventListener('mouseover', function() {
         paris.style.opacity = 1;
@@ -65,6 +100,13 @@ document.addEventListener("DOMContentLoaded", function() {
         paris.style.opacity = 0;
         paris.src = 'images/paris.png';
     });
+    paris.addEventListener('mouseover', function() {
+        parisinfo.style.opacity= 1;
+    });
+    paris.addEventListener('mouseout', function() {
+        parisinfo.style.opacity= 0;
+    });
+
 
     taiwan.addEventListener('mouseover', function() {
         taiwan.style.opacity = 1;
@@ -73,5 +115,11 @@ document.addEventListener("DOMContentLoaded", function() {
     taiwan.addEventListener('mouseout', function() {
         taiwan.style.opacity = 0;
         taiwan.src = 'images/taiwan.png';
+    });
+    taiwan.addEventListener('mouseover', function() {
+        taiwaninfo.style.opacity= 1;
+    });
+    taiwan.addEventListener('mouseout', function() {
+        taiwaninfo.style.opacity= 0;
     });
 });
