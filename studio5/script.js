80d0e6e108db18e8012bfc84dc500b199f426417
angaidesigns <angaidesigns@gmail.com>
2017-03-02 11:22:37 -0800

    update on studio 5
    still can't split the script file

--- studio5/script.js
+++ studio5/script.js
@@ -5,77 +5,76 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // all other js here
     // The amount of symbol we want to place;
-  var count = 50;
 
-  // Create a symbol, which we will use to place instances of later:
-  var path = new Path.Circle({
-  	center: [0, 0],
-  	radius: 5,
-  	fillColor: 'white',
-  	strokeColor: 'black'
-  });
+    var count = 10;
 
-  var symbol = new Symbol(path);
+    // Create a symbol, which we will use to place instances of later:
+    var path = new Path.Circle({
+        center: new Point(0, 0),
+        radius: 30,
+        fillColor: 'white',
+        strokeColor: 'black'
+    });
 
-  // Place the instances of the symbol:
-  for (var i = 0; i < count; i++) {
-  	// The center position is a random point in the view:
-  	var center = Point.random() * view.size;
-  	var placed = symbol.place(center);
-  	placed.scale(i / count + 0.001);
-  	placed.data.vector = new Point({
-  		angle: Math.random() * 360,
-  		length : (i / count) * Math.random() / 5
-  	});
-  }
+    var symbol = new SymbolDefinition(path);
 
-  var vector = new Point({
-  	angle: 45,
-  	length: 0
-  });
+    // Place the instances of the symbol:
+    for (var i = 0; i < count; i++) {
+        // The center position is a random point in the view:
+        var center = Point.random() * view.size;
+        var placed = symbol.place(center);
+        var scale = (i + 1) / count;
+        placed.scale(scale);
+        placed.data.vector = new Point({
+            angle: Math.random() * 360,
+            length : scale * Math.random() / 5
+        });
+    }
 
-  var mouseVector = vector.clone();
+    var vector = new Point({
+        angle: 45,
+        length: 0
+    });
 
-  function onMouseMove(event) {
-  	mouseVector = view.center - event.point;
-  	return false; // Prevent touch scrolling
-  }
+    var mouseVector = vector.clone();
 
-  // The onFrame function is called up to 60 times a second:
-  function onFrame(event) {
-  	vector = vector + (mouseVector - vector) / 30;
+    function onMouseMove(event) {
+        mouseVector = view.center - event.point;
+    }
 
-  	// Run through the active layer's children list and change
-  	// the position of the placed symbols:
-  	for (var i = 0; i < count; i++) {
-  		var item = project.activeLayer.children[i];
-  		var size = item.bounds.size;
-  		var length = vector.length / 10 * size.width / 10;
-  		item.position += vector.normalize(length) + item.data.vector;
-  		keepInView(item);
-  	}
-  }
+    // The onFrame function is called up to 60 times a second:
+    function onFrame(event) {
+        vector = vector + (mouseVector - vector) / 10000;
 
-  function keepInView(item) {
-  	var position = item.position;
-  	var viewBounds = view.bounds;
-  	if (position.isInside(viewBounds))
-  		return;
-  	var itemBounds = item.bounds;
-  	if (position.x > viewBounds.width + 5) {
-  		position.x = -item.bounds.width;
-  	}
+        // Run through the active layer's children list and change
+        // the position of the placed symbols:
+        for (var i = 0; i < count; i++) {
+            var item = project.activeLayer.children[i];
+            var size = item.bounds.size;
+            var length = vector.length / 10 * size.width / 10;
+            item.position += vector.normalize(length) + item.data.vector;
+            keepInView(item);
+        }
+    }
 
-  	if (position.x < -itemBounds.width - 5) {
-  		position.x = viewBounds.width;
-  	}
+    function keepInView(item) {
+        var position = item.position;
+        var itemBounds = item.bounds;
+        var bounds = view.bounds;
+        if (itemBounds.left > bounds.width) {
+            position.x = -item.bounds.width;
+        }
 
-  	if (position.y > viewBounds.height + 5) {
-  		position.y = -itemBounds.height;
-  	}
+        if (position.x < -itemBounds.width) {
+            position.x = bounds.width + itemBounds.width;
+        }
 
-  	if (position.y < -itemBounds.height - 5) {
-  		position.y = viewBounds.height
-  	}
-  }
+        if (itemBounds.top > view.size.height) {
+            position.y = -itemBounds.height;
+        }
+
+        if (position.y < -itemBounds.height) {
+            position.y = bounds.height  + itemBounds.height / 2;
+        }
+    }
 });
