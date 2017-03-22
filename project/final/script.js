2799fece7a60dac107b83f4860703811cdff36b3
angaidesigns <angaidesigns@gmail.com>
2017-03-22 01:49:45 -0700

    overlay for create

--- project/final/script.js
+++ project/final/script.js
@@ -4,3 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
     // all other js here
+    var create = document.getElementById('create');
+    var createcont = document.getElementById('createcont');
+    var close = document.getElementById('close');
+
+    create.addEventListener('click', function(){
+      createcont.style.display="block";
+      // add a fade up?
+    })
+
+    close.addEventListener('click', function(){
+      createcont.style.display="none";
+    })
+})
+
+    // add a cursor hover
