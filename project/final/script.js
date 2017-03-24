a8a1dffe3086193e38e100aa72945cfb265c8fd4
angaidesigns <angaidesigns@gmail.com>
2017-03-23 22:49:51 -0700

    style fix on final

--- project/final/script.js
+++ project/final/script.js
@@ -6,90 +6,85 @@ document.addEventListener("DOMContentLoaded", function() {
     // all other js here
 
 
-      // Initialize Firebase
-      var config = {
-          apiKey: "AIzaSyDbZtAKE47iE8_-MMUDlNQTxDcqV1UtRqQ",
-          authDomain: "des157-e087a.firebaseapp.com",
-          databaseURL: "https://des157-e087a.firebaseio.com",
-          storageBucket: "des157-e087a.appspot.com",
-          messagingSenderId: "790057239919"
-      };
-      firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDbZtAKE47iE8_-MMUDlNQTxDcqV1UtRqQ",
+        authDomain: "des157-e087a.firebaseapp.com",
+        databaseURL: "https://des157-e087a.firebaseio.com",
+        storageBucket: "des157-e087a.appspot.com",
+        messagingSenderId: "790057239919"
+    };
+    firebase.initializeApp(config);
 
 
-      //form
-          var title = document.getElementById("post-title");
-          var name = document.getElementById("post-content");
-          var story = document.getElementById("post-pass");
-         var submitButton = document.getElementById("submit-button");
-         var yourPosts = document.getElementById("your-posts");
-         //var allPosts = document.getElementById("all-posts");
+    //form
+    var title = document.getElementById("post-title");
+    var name = document.getElementById("post-content");
+    var story = document.getElementById("post-pass");
+    var submitButton = document.getElementById("submit-button");
+    var yourPosts = document.getElementById("your-posts");
 
-         submitButton.addEventListener('click', function(){
+    submitButton.addEventListener('click', function() {
 
-           var mainTitle = title.value;
-           var mainContent = name.value;
-           var mainPass = story.value;
+        var mainTitle = title.value;
+        var mainContent = name.value;
+        var mainPass = story.value;
 
-      var d = new Date();
-      var month = d.getMonth() + 1;
-      var date = d.getDate();
-      var year = d.getFullYear();
+        var d = new Date();
+        var month = d.getMonth() + 1;
+        var date = d.getDate();
+        var year = d.getFullYear();
 
-      var article = document.createElement("article");
-      var article1 = document.createElement("article");
+        var article = document.createElement("article");
+        var article1 = document.createElement("article");
 
-      if(anon.checked == true){
+        if (anon.checked == true) {
             article.innerHTML = "<h2>" + mainTitle + "</h2> <h3>Anonymous Poster</h3>  <br><h5>Author</h5> <p>" + mainContent + "</p> <br> <br><h5>Story</h5><p>" + mainPass + "</p> <br><br>";
-            //article1.innerHTML = "<h2>" + mainTitle + "</h2> <h3>Anonymous Poster</h3> <h3>" + month + "-" + date + "-" + year + "</h3> <br><h5>Author</h5> <p>" + mainContent + "</p> <br> <br><h5>I will pass it on by... </h5><p>" + mainPass + "</p> <br><br>";
-          }
-
-      else{
+        } else {
             article.innerHTML = "<h2>" + mainTitle + "</h2>   <br><h5>Author</h5> <p>" + mainContent + "</p> <br><br> <h5>Story</h5><p>" + mainPass + "</p> <br><br>";
-            //article1.innerHTML = "<h2>" + mainTitle + "</h2>  <h3>" + month + "-" + date + "-" + year + "</h3> <br><h5>Author</h5> <p>" + mainContent + "</p> <br><br> <h5>I will pass it on by... </h5><p>" + mainPass + "</p> <br><br>";
-          }
+        }
 
-      yourPosts.insertBefore(article, yourPosts.childNodes[2]);
-      //allPosts.insertBefore(article1, allPosts.childNodes[2]);
-
-      title.value = "";
-       name.value = "";
-       story.value = "";
-
-     });
-
-     function writePost(userId, ptitle, pcontent, ppass) {
-       firebase.database().ref().child("posts").push().set({
-         title: ptitle,
-         content: pcontent,
-         pass: ppass
-       });
-     }
+        yourPosts.insertBefore(article, yourPosts.childNodes[2]);
 
 
-      //create tab
-      var create = document.getElementById('create');
-      var createcont = document.getElementById('createcont');
-      var close = document.getElementById('close');
+        title.value = "";
+        name.value = "";
+        story.value = "";
 
-      create.addEventListener('click', function() {
-          createcont.style.display = "block";
-      })
-      close.addEventListener('click', function() {
-          createcont.style.display = "none";
-      })
+    });
+
+    function writePost(userId, ptitle, pcontent, ppass) {
+        firebase.database().ref().child("posts").push().set({
+            title: ptitle,
+            content: pcontent,
+            pass: ppass
+        });
+    }
+
+
+    //create tab
+    var create = document.getElementById('create');
+    var createcont = document.getElementById('createcont');
+    var close = document.getElementById('close');
+
+    create.addEventListener('click', function() {
+        createcont.style.display = "block";
+    })
+    close.addEventListener('click', function() {
+        createcont.style.display = "none";
+    })
 
 
 
-      // about tab
-      var about = document.getElementById('about');
-      var aboutcont = document.getElementById('aboutcont');
-      var close = document.getElementById('close2');
+    // about tab
+    var about = document.getElementById('about');
+    var aboutcont = document.getElementById('aboutcont');
+    var close = document.getElementById('close2');
 
-      about.addEventListener('click', function() {
-          aboutcont.style.display = "block";
-      })
-      close.addEventListener('click', function() {
-      aboutcont.style.display = "none";
-      })
-      })
+    about.addEventListener('click', function() {
+        aboutcont.style.display = "block";
+    })
+    close.addEventListener('click', function() {
+        aboutcont.style.display = "none";
+    })
+})
