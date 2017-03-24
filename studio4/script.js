ec0566cfc8659a39634d1730e1204bb0ede46c6d
angaidesigns <angaidesigns@gmail.com>
2017-03-24 08:30:43 -0700

    Final update to studio 4

--- studio4/script.js
+++ studio4/script.js
@@ -4,7 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
     // all other js here
-    CountDownTimer('6/17/2017 02:00 PM', 'commence');
+
+
+    CountDownTimer('6/17/2017 09:00 AM', 'commence1');
+    CountDownTimer('6/17/2017 02:00 PM', 'commence2');
+    CountDownTimer('6/17/2017 07:00 PM', 'commence3');
 
 function CountDownTimer(dt, id) {
     var end = new Date(dt);
@@ -35,4 +39,27 @@ function CountDownTimer(dt, id) {
 
     timer = setInterval(showRemaining, 1000);
 }
+
+    var elements = document.getElementsByTagName('li');
+    var tipTimer, tipId;
+
+    for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener ('mouseover', function(event){
+      // what if I wanted to send a parameter ('eat') with the call to showMsg?
+      tipId = document.getElementById(this.id + "Tip");
+      tipTimer = setTimeout(showMsg, 1000);
+    });
+
+    elements[i].addEventListener ('mouseout', function(){
+      clearTimeout(tipTimer);
+      tipId.style.opacity = 0;
+    });
+    }
+
+    function showMsg(){
+      //can I use "this" to find out which object called the function?
+      tipId.style.opacity = 1;
+      tipId.style.transition = 'all 1s';
+    }
+
 });
